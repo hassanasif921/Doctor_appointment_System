369a88b0acd3850a8216ac75b637f4682d1478f9
hassanasif921 <hasssanasif302672@gmail.com>
2020-07-08 19:10:08 +0500

    CRUD on specialist table

---
+++ specialist/create.php
@@ -0,0 +1,41 @@
+<?php 
+ include '../header.php';
+include '../connection.php';
+ $query="Select * from specialist";
+$result=mysqli_query($conn,$query);
+
+?>
+<form method="post">
+
+ <div class="form-group row">
+    <label for="Named" class="col-sm-2 col-form-label">Enter specialization</label>
+    <div class="col-sm-10">
+      <input type="text" class="form-control" id="" placeholder="City Name" name="cna">
+    </div>
+  </div>
+  <div class="form-group row">
+    <div class="col-sm-10">
+      <button type="submit" class="btn btn-primary" name="btnSubmit">Sign in</button>
+    </div>
+  </div>
+</form>
+
+<?php
+include "../footer.php";
+ if(isset($_POST['btnSubmit']))
+{
+   
+
+    $cname=$_POST['cna'];
+    $query1="insert into specialist(specialist) VALUES ('$cname')";
+   $result1=mysqli_query($conn,$query1);
+   //INSERT INTO `doctor`(`d_name`, `d_email`, `d_password`, `image`, `city`, `education`, `availabeid`, `specialization`) VALUES ([value-1],[value-2],[value-3],[value-4],[value-5],[value-6],[value-7],[value-8],[value-9])
+   if($result1)
+   {
+     echo "Successfull";
+   }else{
+    echo "fail";
+   $err= mysqli_error($conn);
+   echo $err;
+   }
+}?>
---
+++ specialist/edit.php
@@ -0,0 +1,47 @@
+<?php 
+$id=$_GET['id'];
+
+include '../connection.php';
+$query="Select * from specialist where id=".$id;
+$result=mysqli_query($conn,$query);
+$row =mysqli_fetch_row($result);
+
+
+
+ if(isset($_POST['btnSubmit']))
+{
+   // $images=$_FILES['images']['tmp_name'];
+  // $img=addslashes(file_get_contents($images));
+    $cname=$_POST['name'];
+   
+  //  $query1="insert into patient(name, city, contact, username, password, Gender) VALUES ('$pname','$citypa','$contactp','$usernamep','$passwordp','$genderps')";
+    $query1=" update specialist set specialist='$cname' where id='$id'";
+   $result1=mysqli_query($conn,$query1);
+   
+   if($result1)
+   {
+       
+    header("Location:index.php");
+}else{
+ echo "fail";
+$err= mysqli_error($conn);
+echo $err;
+}
+}
+include "../adminpanel.php";
+
+?>
+<form method="post">
+  <div class="form-group row">
+    <label for="n" class="col-sm-2 col-form-label">Name</label>
+    <div class="col-sm-10">
+      <input type="text" class="form-control" name="name" id="n"  value="<?php echo $row[1];?>" > 
+    </div>
+</div>
+
+<div class="form-group row">
+    <div class="col-sm-12">
+      <button type="submit" class="btn btn-primary form-control" name="btnSubmit" >Update</button>
+    </div>
+  </div>
+</form>
